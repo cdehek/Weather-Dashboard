57f71baa3a32534d6f9a8fa8e3f978dbb3cd75e2
Christian <christian.dehek@gmail.com>
2020-09-07 21:02:07 -0600

    scrapped old html and made it more simple

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,13 @@
-var cityNameEl = document.getElementById("city-name");
+var cityNameEl = document.querySelector("#city");
+var currentTempEl = document.querySelector("#current-temp");
+var currentWeather = document.querySelector("#current-weather")
 var apiKey = "691b06b89c264940d1a3bb8504966bc2"
 
-// var getCityName = function() {
-//     var cityName = document.getElementById("city-name").value;
 
-// };
-
-var response = fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&units=imperial&appid=" + apiKey)
-.then(function(response) {
+var response = fetch("https://api.openweathermap.org/data/2.5/weather?q=chicago&units=imperial&appid=" + apiKey).then(function(response) {
     response.json().then(function(data) {
         console.log(data);
-    });
+        console.log(data.main.temp);
+    })
 });
 console.log(response);
-
-var formSubmitHandler = function(event) {
-    event.preventDefault();
-    //get city name from input element
-    
-};
-
-var getCityData = function() {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=691b06b89c264940d1a3bb8504966bc2"
-};
-
-
-cityNameEl.addEventListener("submit", formSubmitHandler);
